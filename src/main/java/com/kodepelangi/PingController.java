a63c2555e0724afcd95d0cd7ae5a3d5deeeaf0d3
Raka Teja <rakatejaa@gmail.com>
2015-01-06 21:23:33 +0800

    ping testing

--- src/main/java/com/kodepelangi/PingController.java
+++ src/main/java/com/kodepelangi/PingController.java
@@ -22,7 +22,7 @@ public class PingController extends HttpServlet{
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
         PrintWriter writer = response.getWriter();
-        writer.println("<html>Ping</html>");
+        writer.println("<html>Pong</html>");
         writer.flush();
     }
 
