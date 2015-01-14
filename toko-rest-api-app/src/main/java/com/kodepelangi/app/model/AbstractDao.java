615631cfd9bb7901ebe658feae14220c56524e43
Raka Teja <rakatejaa@gmail.com>
2015-01-14 23:19:29 +0800

    add add entity method in parent dao class

--- toko-rest-api-app/src/main/java/com/kodepelangi/app/model/AbstractDao.java
+++ toko-rest-api-app/src/main/java/com/kodepelangi/app/model/AbstractDao.java
@@ -1,13 +1,12 @@
 package com.kodepelangi.app.model;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.List;
 
 /**
  * @author Raka Teja<rakatejaa@gmail.com>
  */
-public class AbstractDao {
+public abstract class AbstractDao {
 
     protected Connection conn;
 
@@ -17,6 +16,26 @@ public class AbstractDao {
 
     public AbstractDao(Connection connection){
         this.conn = connection;
+    }
+
+    protected int add(String query, String[] params){
+        try {
+            this.preparedStatement = this.conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+            for (int i=1;i<params.length+1;i++){
+                this.preparedStatement.setString(i, params[i - 1]);
+            }
+
+            if(this.preparedStatement.executeUpdate()==1){
+                this.resultSet = this.preparedStatement.getGeneratedKeys();
+                if(this.resultSet.next()){
+                    return this.resultSet.getInt(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
 }
