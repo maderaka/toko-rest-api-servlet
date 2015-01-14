90d2f02b1cc61286a60983f51d025c11011e6ab0
Raka Teja <rakatejaa@gmail.com>
2015-01-15 00:00:08 +0800

    set pojo objcet

--- toko-rest-api-product/src/main/java/com/kodepelangi/product/model/dao/impl/ProductDao.java
+++ toko-rest-api-product/src/main/java/com/kodepelangi/product/model/dao/impl/ProductDao.java
@@ -20,11 +20,18 @@ public class ProductDao extends AbstractDao implements ProductDaoInterface {
     }
 
     @Override
-    public int create(Product obj) {
+    public int create(Product p) {
         int lastIdInserted = 0;
         try {
             this.query = "INSERT INTO `product`(`code`,`title`,`price`,`desc`,`status`,`brand_id`,`category_id`) VALUES(?,?,?,?,?,?,?)";
             this.preparedStatement = this.conn.prepareStatement(this.query, Statement.RETURN_GENERATED_KEYS);
+            this.preparedStatement.setString(1, p.getCode());
+            this.preparedStatement.setString(2, p.getTitle());
+            this.preparedStatement.setBigDecimal(3, p.getPrice());
+            this.preparedStatement.setString(4,p.getDesc());
+            this.preparedStatement.setInt(5, p.getStatus());
+            this.preparedStatement.setInt(6, p.getBrand().getId());
+            this.preparedStatement.setInt(7, p.getCategory().getId());
             return this.add();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -33,10 +40,19 @@ public class ProductDao extends AbstractDao implements ProductDaoInterface {
     }
 
     @Override
-    public boolean update(Product obj) {
+    public boolean update(Product p) {
         try {
             this.query = "UPDATE `brand` SET `code`=?, `title`=?,`price`=?,`desc`=?,`status`=?,`brand_id`=?,`category`=? WHERE `id`=?";
             this.preparedStatement = this.conn.prepareStatement(this.query);
+
+            this.preparedStatement.setString(1, p.getCode());
+            this.preparedStatement.setString(2, p.getTitle());
+            this.preparedStatement.setBigDecimal(3, p.getPrice());
+            this.preparedStatement.setString(4,p.getDesc());
+            this.preparedStatement.setInt(5, p.getStatus());
+            this.preparedStatement.setInt(6, p.getBrand().getId());
+            this.preparedStatement.setInt(7, p.getCategory().getId());
+
             return this.preparedStatement.executeUpdate() ==1;
         } catch (SQLException e) {
             e.printStackTrace();
